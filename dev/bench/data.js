74bcd27f062005229526ce3d2533c01f9bda063b
github-action-benchmark <github@users.noreply.github.com>
2024-02-27 08:00:57 +0000

    add Benchmark (benchmarkjs) benchmark result for 2dc0de86bf1c2add57b95edab571f34b75d86c44

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707202984907,
+  "lastUpdate": 1709020856798,
   "repoUrl": "https://github.com/zondervancalvez/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.93%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "2dc0de86bf1c2add57b95edab571f34b75d86c44",
+          "message": "build(deps-dev): bump vite in /packages/cacti-ledger-browser-react\n\nBumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 5.0.8 to 5.0.12.\n- [Release notes](https://github.com/vitejs/vite/releases)\n- [Changelog](https://github.com/vitejs/vite/blob/v5.0.12/packages/vite/CHANGELOG.md)\n- [Commits](https://github.com/vitejs/vite/commits/v5.0.12/packages/vite)\n\n---\nupdated-dependencies:\n- dependency-name: vite\n  dependency-type: direct:development\n...\n\nCo-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>\n\nSigned-off-by: dependabot[bot] <support@github.com>\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-02-26T17:55:29-08:00",
+          "tree_id": "e8d108948d869f1173857dfb0e1770faed743f5e",
+          "url": "https://github.com/zondervancalvez/cactus/commit/2dc0de86bf1c2add57b95edab571f34b75d86c44"
+        },
+        "date": 1709020854789,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 610,
+            "range": "±1.71%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 385,
+            "range": "±1.55%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
           }
         ]
       }
