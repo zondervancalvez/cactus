548cb0b86184dcd595975781c344f14876890c4a
github-action-benchmark <github@users.noreply.github.com>
2024-04-16 06:45:51 +0000

    add Benchmark (benchmarkjs) benchmark result for acca13e0d6b6589d6c0efb8d78bbfdfb4ec91377

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712135990907,
+  "lastUpdate": 1713249951377,
   "repoUrl": "https://github.com/zondervancalvez/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.13%",
             "unit": "ops/sec",
             "extra": "182 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "distinct": true,
+          "id": "acca13e0d6b6589d6c0efb8d78bbfdfb4ec91377",
+          "message": "Merge branch 'main' of https://github.com/zondervancalvez/cactus into main",
+          "timestamp": "2024-04-16T14:29:28+08:00",
+          "tree_id": "461594c31d3cfc53eb6e0654040483304488f172",
+          "url": "https://github.com/zondervancalvez/cactus/commit/acca13e0d6b6589d6c0efb8d78bbfdfb4ec91377"
+        },
+        "date": 1713249948188,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 576,
+            "range": "±1.82%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 362,
+            "range": "±1.25%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
