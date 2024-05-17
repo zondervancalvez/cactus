ebbe9cfcdc2691bae3c454ab08b2ddacf7045a14
github-action-benchmark <github@users.noreply.github.com>
2024-05-17 06:29:29 +0000

    add Benchmark (benchmarkjs) benchmark result for caa2b3a50ab7928942e72341ebc7e5511e3054f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715926298605,
+  "lastUpdate": 1715927368847,
   "repoUrl": "https://github.com/zondervancalvez/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.37%",
             "unit": "ops/sec",
             "extra": "180 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sandeep.nishad1@ibm.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "caa2b3a50ab7928942e72341ebc7e5511e3054f7",
+          "message": "fix(corda-simple-app): use correct bond asset flows and contracts for bond asset exchange\n\n    corda-simple-app: fix typo for partyb twice in initNetworkId.sh\n\nSigned-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>",
+          "timestamp": "2024-05-16T21:25:53-07:00",
+          "tree_id": "93b26c979a99de08a324dc8c2802d4002c0bd719",
+          "url": "https://github.com/zondervancalvez/cactus/commit/caa2b3a50ab7928942e72341ebc7e5511e3054f7"
+        },
+        "date": 1715927366035,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 740,
+            "range": "±2.68%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
