03b05cd4422652299af0e771f936677ac30488c4
github-action-benchmark <github@users.noreply.github.com>
2024-05-27 08:12:38 +0000

    add Benchmark (benchmarkjs) benchmark result for 6994e5b7a43b4f3e06535babb17edd466c4d4abc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716796519285,
+  "lastUpdate": 1716797557891,
   "repoUrl": "https://github.com/zondervancalvez/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.44%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sandeep.nishad1@ibm.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "6994e5b7a43b4f3e06535babb17edd466c4d4abc",
+          "message": "fix(weaver-go-sdk): revert fabric-protos-go-apiv2 dep to fabric-protos-go\n\n    * Added a script to manually change go package names inside fabric-protos\n      to fabric-protos-go-apiv2 (for future migration apiv2).\n    * Added more unit and build tests covering all go modules\n    * Added tools/go-mod-tidy.sh script to fix go.mod by running go mod tidy\n\nSigned-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>",
+          "timestamp": "2024-05-24T12:02:26-07:00",
+          "tree_id": "d362d441cc6cadaf31b7851a6c4578c64fe3adcd",
+          "url": "https://github.com/zondervancalvez/cactus/commit/6994e5b7a43b4f3e06535babb17edd466c4d4abc"
+        },
+        "date": 1716797555528,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 737,
+            "range": "±2.91%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
           }
         ]
       }
